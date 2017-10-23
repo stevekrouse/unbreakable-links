88c1ae9e4f84fe30cb75577d8fca66ab2e3b773d
Steve Krouse <skrouse@seas.upenn.edu>
2017-10-23 22:20:55 +0000

    more...

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ const load = (repoPath, filePath, commitHash) => {
   iframe.src = "https://cdn.rawgit.com/" + repoPath + "/" + commitHash + "/" + filePath
   iframe.height = "100%"
   iframe.width = "100%"
-  iframe.styles.border = "none"
-  iframe.styles.padding = "none"
-  iframe.styles.margin = "none"
+  iframe.style.border = "none"
+  document.body.style.margin = "0px"
   document.body.innerHTML = '';
   document.body.appendChild(iframe)
 }
@@ -19,15 +18,23 @@ const putCommitHashInURL = commitHash => {
   window.location.hash = commitHash
 }
 
-const getMostRecentCommitHash = repoPath => fetch(
+const getMostRecentCommitHash = (repoPath, filePath) => fetch(
   'https://exec.clay.run/steve/github-project-data', {
     method: "POST",
     body: JSON.stringify({
       repo: repoPath,
+      path: filePath
     })
   })
   .then(response => response.json())
   .then(json => json[0].sha)
+
+
+
+
+const showBanner = (repoPath, filePath, status) => {
+  
+}
 
 window.addEventListener("load", () => {
   const commitHashInURL = window.location.hash.slice(1)
@@ -39,9 +46,18 @@ window.addEventListener("load", () => {
   const repoNameIndexInURLPath = window.location.pathname.indexOf(repoName + "/")
   const filePath = repoNameIndexInURLPath == -1 ? window.location.pathname : window.location.pathname.substring(repoNameIndexInURLPath + repoName.length + 1)
 
-  if (commitHashInURL) {
-    load(repoPath, filePath, commitHashInURL)
-  } else {
-    getMostRecentCommitHash(repoPath).then(putCommitHashInURL)
-  }
+  getMostRecentCommitHash.then(mostRecentCommitHash => {
+    if (commitHashInURL && commitHashInURL != mostRecentCommitHash) {
+      load(repoPath, filePath, commitHashInURL)
+      showBanner(repoPath, filePath, "NEWER-VERSION-AVAILABLE")
+    } else {
+      if (!commitHashInURL) {
+        putCommitHashInURL(mostRecentCommitHash)
+      }
+      showBanner(repoPath, filePath, "UP-TO-DATE")
+    }
+  }).catch(() => {
+    // we will end up here on 404.html 
+    // if there's never been such a filePath on this repoName
+  })
 })
