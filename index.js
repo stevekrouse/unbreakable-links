8a4db6657c1bd91f7fe7eb4c12d19022810dfa42
Steve Krouse <skrouse@seas.upenn.edu>
2017-11-06 23:11:58 +0000

    added icons for deleted, up-to-date and newer-version-available

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// icons created by https://smashicons.com/
+const deletedFileSVGText = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 58 58" style="enable-background:new 0 0 58 58;" xml:space="preserve"><g><g><polygon style="fill:#EFEBDE;" points="46.5,14 32.5,0 1.5,0 1.5,58 46.5,58 "/><g><path style="fill:#D5D0BB;" d="M11.5,23h25c0.552,0,1-0.447,1-1s-0.448-1-1-1h-25c-0.552,0-1,0.447-1,1S10.948,23,11.5,23z"/><path style="fill:#D5D0BB;" d="M11.5,15h10c0.552,0,1-0.447,1-1s-0.448-1-1-1h-10c-0.552,0-1,0.447-1,1S10.948,15,11.5,15z"/><path style="fill:#D5D0BB;" d="M36.5,29h-25c-0.552,0-1,0.447-1,1s0.448,1,1,1h25c0.552,0,1-0.447,1-1S37.052,29,36.5,29z"/><path style="fill:#D5D0BB;" d="M36.5,37h-25c-0.552,0-1,0.447-1,1s0.448,1,1,1h25c0.552,0,1-0.447,1-1S37.052,37,36.5,37z"/><path style="fill:#D5D0BB;" d="M36.5,45h-25c-0.552,0-1,0.447-1,1s0.448,1,1,1h25c0.552,0,1-0.447,1-1S37.052,45,36.5,45z"/></g><polygon style="fill:#D5D0BB;" points="32.5,0 32.5,14 46.5,14 "/></g><g><circle style="fill:#ED7161;" cx="44.5" cy="46" r="12"/><path style="fill:#FFFFFF;" d="M45.914,46l3.536-3.536c0.391-0.391,0.391-1.023,0-1.414s-1.023-0.391-1.414,0L44.5,44.586 l-3.536-3.536c-0.391-0.391-1.023-0.391-1.414,0s-0.391,1.023,0,1.414L43.086,46l-3.536,3.536c-0.391,0.391-0.391,1.023,0,1.414 c0.195,0.195,0.451,0.293,0.707,0.293s0.512-0.098,0.707-0.293l3.536-3.536l3.536,3.536c0.195,0.195,0.451,0.293,0.707,0.293 s0.512-0.098,0.707-0.293c0.391-0.391,0.391-1.023,0-1.414L45.914,46z"/></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>'
+const upToDateFileSVGText = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 58 58" style="enable-background:new 0 0 58 58;" xml:space="preserve"><g><g><polygon style="fill:#EFEBDE;" points="46.5,14 32.5,0 1.5,0 1.5,58 46.5,58   "/><g><path style="fill:#D5D0BB;" d="M11.5,23h25c0.552,0,1-0.447,1-1s-0.448-1-1-1h-25c-0.552,0-1,0.447-1,1S10.948,23,11.5,23z"/><path style="fill:#D5D0BB;" d="M11.5,15h10c0.552,0,1-0.447,1-1s-0.448-1-1-1h-10c-0.552,0-1,0.447-1,1S10.948,15,11.5,15z"/><path style="fill:#D5D0BB;" d="M36.5,29h-25c-0.552,0-1,0.447-1,1s0.448,1,1,1h25c0.552,0,1-0.447,1-1S37.052,29,36.5,29z"/><path style="fill:#D5D0BB;" d="M36.5,37h-25c-0.552,0-1,0.447-1,1s0.448,1,1,1h25c0.552,0,1-0.447,1-1S37.052,37,36.5,37z"/><path style="fill:#D5D0BB;" d="M36.5,45h-25c-0.552,0-1,0.447-1,1s0.448,1,1,1h25c0.552,0,1-0.447,1-1S37.052,45,36.5,45z"/></g><polygon style="fill:#D5D0BB;" points="32.5,0 32.5,14 46.5,14   "/></g><g><circle style="fill:#26B999;" cx="44.5" cy="46" r="12"/><path style="fill:#FFFFFF;" d="M51.071,40.179c-0.455-0.316-1.077-0.204-1.392,0.25l-5.596,8.04l-3.949-3.242    c-0.426-0.351-1.057-0.288-1.407,0.139c-0.351,0.427-0.289,1.057,0.139,1.407l4.786,3.929c0.18,0.147,0.404,0.227,0.634,0.227    c0.045,0,0.091-0.003,0.137-0.009c0.276-0.039,0.524-0.19,0.684-0.419l6.214-8.929C51.636,41.118,51.524,40.495,51.071,40.179z"/></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>'
+const newerVersionAvailFileSvg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 58 58" style="enable-background:new 0 0 58 58;" xml:space="preserve" width="512" height="512" class=""><g><g><g><polygon style="fill:#EFEBDE;" points="46,14 32,0 1,0 1,58 46,58   " data-original="#EFEBDE" class=""></polygon><g><path style="fill:#D5D0BB;" d="M11,23h25c0.552,0,1-0.447,1-1s-0.448-1-1-1H11c-0.552,0-1,0.447-1,1S10.448,23,11,23z" data-original="#D5D0BB" class=""></path><path style="fill:#D5D0BB;" d="M11,15h10c0.552,0,1-0.447,1-1s-0.448-1-1-1H11c-0.552,0-1,0.447-1,1S10.448,15,11,15z" data-original="#D5D0BB" class=""></path><path style="fill:#D5D0BB;" d="M36,29H11c-0.552,0-1,0.447-1,1s0.448,1,1,1h25c0.552,0,1-0.447,1-1S36.552,29,36,29z" data-original="#D5D0BB" class=""></path><path style="fill:#D5D0BB;" d="M36,37H11c-0.552,0-1,0.447-1,1s0.448,1,1,1h25c0.552,0,1-0.447,1-1S36.552,37,36,37z" data-original="#D5D0BB" class=""></path><path style="fill:#D5D0BB;" d="M36,45H11c-0.552,0-1,0.447-1,1s0.448,1,1,1h25c0.552,0,1-0.447,1-1S36.552,45,36,45z" data-original="#D5D0BB" class=""></path></g><polygon style="fill:#D5D0BB;" points="32,0 32,14 46,14   " data-original="#D5D0BB" class=""></polygon></g><g><circle style="fill:#F29C21" cx="46" cy="47" r="11" data-original="#76C9B0" class="active-path" data-old_color="#76C9B0"></circle><path style="fill:#FFFFFF" d="M52,46h-5v-5c0-0.552-0.447-1-1-1s-1,0.448-1,1v6c0,0.552,0.447,1,1,1h6c0.553,0,1-0.448,1-1    S52.553,46,52,46z" data-original="#FFFFFF" class="" data-old_color="#F29C21"></path></g></g></g></svg>'
+
 const repoPathFromScriptAttribute = () => {
   const currentScript = document.currentScript || Array.prototype.slice.call(document.getElementsByTagName('script')).find(s => s.src.includes('unbreakable-links'))
   return currentScript.getAttribute('repoPath')
@@ -8,10 +13,9 @@ const rawGitLink = (repoPath, filePath, commitHash) => "https://cdn.rawgit.com/"
 const load = (repoPath, filePath, commitHash) => {
   const iframe = document.createElement("iframe")
   iframe.src = rawGitLink(repoPath, filePath, commitHash)
-  iframe.height = "100%"
-  iframe.width = "100%"
+  iframe.style.height = "100%"
+  iframe.style.width = "100%"
   iframe.style.border = "none"
-  document.body.style.margin = "50 0 0 0px"
   document.body.innerHTML = '';
   document.body.appendChild(iframe)
 }
@@ -44,6 +48,16 @@ const showBanner = (repoPath, filePath, status) => {
     "NEWER-VERSION-AVAILABLE": `There is a newer version of this file available.`,
     "DELETED": "This file has been deleted. You are viewing an archived version."
   }[status]
+  
+  const iconDiv = document.createElement('span')
+  iconDiv.style.width = "30px"
+  iconDiv.style.display = "inline-block"
+  iconDiv.innerHTML = {
+    "FILE-NEVER-EXISTED": deletedFileSVGText, // TODO
+    "UP-TO-DATE": upToDateFileSVGText, 
+    "NEWER-VERSION-AVAILABLE": newerVersionAvailFileSvg, 
+    "DELETED": deletedFileSVGText
+  }[status]
   const bannerCSS = [
     "z-index:2", 
     "position:absolute", 
@@ -53,13 +67,13 @@ const showBanner = (repoPath, filePath, status) => {
     "display: flex",
     "align-items: center",
     "justify-content: center",
-    "border: 1px solid lightcyan",
-    "background-color: lightblue",
+    // "border: 1px solid lightcyan",
+    // "background-color: lightblue",
     "font-family: sans-serif"
  ]
   const bannerHTML = `<div style="${bannerCSS.join(";")}">
      <div>
-       ${bannerText}
+       ${iconDiv.outerHTML + bannerText}
      </div>
    </div>`
   document.body.innerHTML += bannerHTML
